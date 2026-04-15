626cae73bee6bb687ac1685eb6fb6835db530440
Jesus Obispo <jesus.obispo@educa.madrid.org>
2026-04-15 12:32:01 +0200

    post

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import db from 'mongoose';
 import express from 'express';
 
 const app = express();
-
+app.use(express.json())
 const port = 3000;
 
 app.get('/', (req, res)=> {
-
     res.send("hola mundo")
 });
+
+
+app.post('/holi',(req, res)=>{
+    console.log(req.body)
+    const {nombre}=req.body
+    res.send(`hola ${nombre}`)
+})
 
 app.listen(port, ()=>{
     console.log(`el servidor aranco en la url http://localhost:${port}`)
